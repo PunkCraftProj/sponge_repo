e76b2903d5da5dc54bcb79ebc66702f3b6f3eb6e
OpexHunter <ivan.vekhov@mail.ru>
2024-06-08 18:16:53 +0300

    install bat fix windows

--- build/Windows/install.bat
+++ build/Windows/install.bat
@@ -1,7 +1,7 @@
 @echo off
 REM COPY
-cd ..\..\
-cd ..\target
+
+cd ..\..\target
 copy /Y OneWorldSponge-0.1-SNAPSHOT.jar ..\..\ServerOneWorld\Server1\mods
 copy /Y OneWorldSponge-0.1-SNAPSHOT.jar ..\..\ServerOneWorld\Server2\mods
 
